5a907fa0b100b45031b11106f00595299ff2d3e0
curtisnlogan <curtisnlogan@gmail.com>
2026-06-03 15:59:10 +0100

    chore: add __str__ method to GameRecord class

--- game_record.py
+++ game_record.py
@@ -8,7 +8,6 @@ import uuid
 
 
 class GameRecord:
-
     __game_id: str
     __title: str
     __platform: str
@@ -16,7 +15,13 @@ class GameRecord:
     # fixed replacement cost to $40.00 as per the requirements
     REPLACEMENT_COST: float = 40.00
     # stops the use of abbreviations for platforms and ensures that only valid platforms are used
-    VALID_PLATFORMS = ["xbox", "playstation_1", "playstation_2", "n64", "nintendo_gamecube"]
+    VALID_PLATFORMS = [
+        "xbox",
+        "playstation_1",
+        "playstation_2",
+        "n64",
+        "nintendo_gamecube",
+    ]
 
     def __init__(
         self,
@@ -29,47 +34,61 @@ class GameRecord:
         self.__title = title
         self.__platform = platform
         self.__total_copies = total_copies
-        self.__replacement_cost = replacement_cost
-    
+        self.__replacement_cost = GameRecord.REPLACEMENT_COST
+
+    def __str__(self):
+        return (
+            f"This game record references the game_id of {self.game_id}.\n"
+            f"The title is {self.title} for the {self.platform} platform.\n"
+            f"There are currently {self.total_copies} total copies of this game in-stock.\n"
+            f"The replacement cost for all games is currently {GameRecord.REPLACEMENT_COST}"
+        )
+
     # does not have setter method as this value should not be changed after the game record is created
     @property
     def game_id(self):
         return self.__game_id
-    
+
     @property
     def title(self):
         return self.__title
-    
+
     @title.setter
     def title(self, new_title: str):
         if new_title is not None and new_title != "":
-           self.__title = new_title.strip().lower()
+            self.__title = new_title.strip().lower()
         else:
             raise ValueError("Title cannot be 'None' or an empty string")
 
     @property
     def platform(self):
         return self.__platform
-    
+
     @platform.setter
     def platform(self, new_platform: str):
-        if new_platform is not None and new_platform != "" and new_platform in GameRecord.VALID_PLATFORMS:
+        if (
+            new_platform is not None
+            and new_platform != ""
+            and new_platform in GameRecord.VALID_PLATFORMS
+        ):
             self.__platform = new_platform.strip().lower()
         else:
-            raise ValueError("Platform cannot be 'None', an empty string or an invalid platform")
+            raise ValueError(
+                "Platform cannot be 'None', an empty string or an invalid platform"
+            )
 
     @property
     def total_copies(self):
         return self.__total_copies
-    
+
     @total_copies.setter
     def total_copies(self, new_total_copies: int):
         if isinstance(new_total_copies, int) and 0 <= new_total_copies < 100:
             self.__total_copies = new_total_copies
         else:
             raise ValueError("Total copies must be between 0 and 99 inclusive")
+
     # does not have setter method as this value should not be changed on a per game basis
     @property
     def replacement_cost(self):
         return self.__replacement_cost
-    
