1437a23f035d77115dc373143193716379c85cae
curtisnlogan <curtisnlogan@gmail.com>
2026-07-06 15:59:47 +0100

    feat: add variables for JSON file paths in config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+"""
+This file contains paths to the json files used in the this application.
+Allows for centralized management of file paths, making it easier to update paths in one place if needed.
+"""
+
+MEMBERS_PATH = "data/members.json"
+RENTALS_PATH = "data/rentals.json"
+GAME_RECORDS_PATH = "data/game_records.json"
