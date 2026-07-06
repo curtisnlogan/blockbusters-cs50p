7e57ceaa92a699bab36fb6570818fb8b41e4c6cb
curtisnlogan <curtisnlogan@gmail.com>
2026-07-06 17:04:59 +0100

    test: add unit tests for GameRecord, Membership, and RentalRecord classes

--- test_project.py
+++ test_project.py
@@ -1,10 +1,20 @@
-def test_function_1():
-    ...
+from game_record import GameRecord
+from membership import Membership
+from rental_record import RentalRecord
+
+import pytest
+
+def test_GameRecord():
+    GameRecord1 = GameRecord("  Hogs of War   ", "playstation_1", 10)
+    assert GameRecord1.title == "hogs of war"
 
 
-def test_function_2():
-    ...
+def test_Membership():
+    Membership1 = Membership("H.P. Lovecraft", True, "   66 college Street in providence, rhode island   ", "Credit Card")
+    assert Membership1.address == "66 College Street In Providence, Rhode Island"
 
 
-def test_function_n():
-    ...
+def test_RentalRecord():
+    RentalRecord1 = RentalRecord("a1a34c72-9dd2-41c3-8a79-6d06c7970b4b", "dcbc1fd1-bdd1-4435-a09f-e59e63b6b604")
+    with pytest.raises(ValueError):
+        RentalRecord1.membership_id = ""
