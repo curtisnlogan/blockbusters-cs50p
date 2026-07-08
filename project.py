9f572e15b15bb33a35b4884a33c73aa5140170ce
curtisnlogan <curtisnlogan@gmail.com>
2026-07-08 16:38:45 +0100

    feat: implement main function to load JSON data from configured paths

--- project.py
+++ project.py
@@ -1,4 +1,15 @@
-def main(): ...
+import config
+import storage
+
+def main():
+    # dict defines the paths to the json files for each storage type
+    storage_dict = {
+        "game_records": config.GAME_RECORDS_PATH,
+        "members": config.MEMBERS_PATH,
+        "rentals": config.RENTALS_PATH,
+        }
+    
+    in_memory_storage = storage.load_jsons(**storage_dict)
 
 
 if __name__ == "__main__":
