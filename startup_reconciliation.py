5c90f64d34f60d5e85ba46aaccb9bffeb1be16de
curtisnlogan <curtisnlogan@gmail.com>
2026-07-08 18:07:56 +0100

    feat: add initial logic for rental overdue checks in startup_reconcile function

--- startup_reconciliation.py
+++ startup_reconciliation.py
@@ -1,8 +1,25 @@
+from datetime import date
+
 def startup_reconile(data: dict) -> dict:
     """
     Perform startup reconciliation on the the in-memory data dictionary. 
     This function checks late fees and replacement fees, and updates the data accordingly.
     It returns the reconciled dictionary.
     """
+
+    for rental in data["rentals"].items():
+        if rental["rental_status"] == "rented":
+
+            # convert to str to date obj
+            due_obj = date.fromisoformat(rental["due_for_return"])
+
+            # need to check if its more or less than 14 days overdue
+
+            # if under 14 days, add $1 dollar to late fee total per day overdue
+
+            # else if over 14 days, `replacement_charge` is false, set `replacement_charge` to `true`, 
+            # reset `late_fees_total` to `0.0`, set `return_status` to `lost`, deduct `1` from the corresponding game's `total_copies`, 
+            # and block the associated member's account
+
     # Placeholder logic: simply return the input data
     return data
