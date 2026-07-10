773ac302b5e5a567e6dfa1a4b546c9f7530ff4ac
curtisnlogan <curtisnlogan@gmail.com>
2026-07-10 17:24:00 +0100

    feat: partial implementation of main menu and game records display in CLI

---
+++ cli.py
@@ -0,0 +1,50 @@
+from rich.console import  Console
+from rich.table import Table
+
+console = Console()
+
+def main_menu():
+    """
+    Displays the main menu and prompts the user for input.
+    Returns the user's choice as a string.
+    """
+    print("\nWelcome to the Blockbusters CLI! All data has been loaded into memory. Please select an option from the menu below:")
+    print("\nMain Menu:")
+    print("1. Game Records")
+    print("2. Members")
+    print("3. Rentals")
+    print("4. Exit")
+
+
+    choice = input("Enter your choice (1-4): ")
+    if choice not in ["1", "2", "3", "4"]:
+        raise ValueError("Invalid choice. Please enter a number precisely between 1 and 4.")
+    elif choice == 2:
+        print("You selected Members. This feature is not yet implemented.")
+        return choice
+    elif choice == 3:
+        print("You selected Rentals. This feature is not yet implemented.")
+        return choice
+    elif choice == 4:
+        print("Exiting the program. Goodbye!")
+        return choice
+
+def view_game_records(game_records):
+
+    # create a table to display game records
+    table = Table(title="Game Records")
+
+    #define columns
+    table.add_column("Game ID", justify="right")
+    table.add_column("Title", justify="right")
+    table.add_column("Platform", justify="right")
+    table.add_column("Total Copies", justify="right")
+    table.add_column("Replacement Cost", justify="right")
+
+    #build rows
+    for game in game_records:
+        table.add_row(game_records["game_id"], game_records["title"], game_records["platform"], 
+                      str(game_records["total_copies"]), str(game_records["replacement_cost"]))
+
+    #end
+    console.print(table)
