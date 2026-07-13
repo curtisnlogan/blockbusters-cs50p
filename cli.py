290e3225b4c00756dfe5a20601091c4ed9ef845c
curtisnlogan <curtisnlogan@gmail.com>
2026-07-13 18:02:01 +0100

    feat: enhance main menu with exit confirmation and fix user input handling

--- cli.py
+++ cli.py
@@ -22,23 +22,27 @@ def main_menu():
         console.print("3. Rentals Management")
         console.print("4. Exit the Program")
 
-    choice = input("Enter your choice (1-4): ")
-    if choice not in ["1", "2", "3", "4"]:
-        raise ValueError(
-            "Invalid choice. Please enter a number exactly between 1 and 4."
-        )
-    elif choice == 1:
-        console.print("You selected Game Records.")
-        return choice
-    elif choice == 2:
-        console.print("You selected Members.")
-        return choice
-    elif choice == 3:
-        console.print("You selected Rentals.")
-        return choice
-    elif choice == 4:
-        console.print("Exiting the program.")
-        return choice
+        choice = input("Enter your choice (1-4): ")
+        if choice not in ["1", "2", "3", "4"]:
+            raise ValueError(
+                "Invalid choice. Please enter a number exactly between 1 and 4."
+            )
+        elif choice == "1":
+            console.print("You selected Game Records.")
+            return choice
+        elif choice == "2":
+            console.print("You selected Members.")
+            return choice
+        elif choice == "3":
+            console.print("You selected Rentals.")
+            return choice
+        elif choice == "4":
+            confirm_exit = input("Are you sure you want to exit? (y/n): ").strip().lower()
+            if confirm_exit == "y":
+                console.print("Exiting the program.")
+                return choice
+            else:
+                continue  # Return to the main menu if the user does not confirm exit
 
 
 def view_game_records(game_records: dict):
