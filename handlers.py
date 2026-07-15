a1798759f742b82f64beeca86c30124f13eea77c
curtisnlogan <curtisnlogan@gmail.com>
2026-07-15 12:37:26 +0100

    feat: implement rental generation logic in handle function

--- handlers.py
+++ handlers.py
@@ -1,6 +1,7 @@
 from datetime import date
 
 import cli
+from rental_record import RentalRecord
 import startup_reconciliation
 import sys
 
@@ -38,6 +39,7 @@ def handle(data: dict) -> dict:
                 rentals = data["rentals"]
                 try:
                     new_rentals = cli.rent_games(rentals)
+                    new_rentals = generate_new_rentals(data, new_rentals)
                 except (ValueError, KeyError) as e:
                     print(e)
                     continue  # Return to the main menu if there's an error
@@ -51,3 +53,18 @@ def handle(data: dict) -> dict:
                 continue  # Return to the main menu
         if selection == "4":
             sys.exit()  # Exit the program
+    
+def generate_new_rentals(data:dict, new_rentals: tuple) -> list:
+    """
+    Generates new rental instances based on the provided Game IDs and Member ID.
+    Updates the total copies of the games in the in-memory data store.
+    Returns a list of new RentalRecord instances.
+    """
+    member_id = new_rentals[1]
+    new_rental_instances = []
+    for game_id in new_rentals[0]:
+        new_rental_instances.append(RentalRecord(game_id, member_id))
+        data["game_records"][game_id]["total_copies"] -= 1
+
+    return new_rental_instances
+
